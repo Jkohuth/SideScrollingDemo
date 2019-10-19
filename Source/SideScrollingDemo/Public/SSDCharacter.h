78892ddc0c90b1d16a8c54740c1e06c14a65290c
Jacob Kohuth <jkohuth@github.com>
2019-10-18 20:37:30 -0600

    Character component

--- Source/SideScrollingDemo/Public/SSDCharacter.h
+++ Source/SideScrollingDemo/Public/SSDCharacter.h
@@ -32,6 +32,8 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CameraBounds", meta = (AllowPrivateAccess = "true"))
 	class UCameraBoundingBoxComponent* CameraBounds;
 
+	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category = Direction)
+	class UArrowComponent* ArrowComponent;
 	// Handle Axis Input
 	void MoveRight(float Value);
 	void MoveUp(float Value);
@@ -49,7 +51,7 @@ public:
 	UFUNCTION()
 	void OnActorOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 	UFUNCTION()
-		void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 	// JUMP - Platformer must have satisfying vertical movment
 	virtual void Jump() override;
@@ -104,6 +106,14 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "MalePlayer")
 	virtual ECharacterState GetCharacterState();
 	
+	UFUNCTION(BlueprintCallable, Category = "Pawn|Character")
+	bool IsGrinding() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Pawn|Character")
+	bool IsClimbing() const;
+
+
+
 	// BACK DASH - needs button delay
 	UFUNCTION(BlueprintCallable, Category = "MalePlayer")
 	void BackDash();
@@ -124,6 +134,9 @@ public:
 	void OnStartSlide();
 	// Event called when player releases slide button
 	void OnStopSlide();
+
+	FORCEINLINE class UArrowComponent* GetArrowComponent const { return ArrowComponent; }
+
 
 private:
 	uint32 bPressedSlide:1;
--- Source/SideScrollingDemo/Public/SSDPlayerMovementComponent.h
+++ Source/SideScrollingDemo/Public/SSDPlayerMovementComponent.h
@@ -13,5 +13,76 @@ UCLASS()
 class SIDESCROLLINGDEMO_API USSDPlayerMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
+
+protected:
+	float capsuleHalfHeight;
+	float capsuleRadius;
+	float capsuleRadius;
+	float wallLinesCount;
+	float wallLinesSpace;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Rail)
+		class USplineComponent* RailSplineReference;
+
 	
+	TArray<bool> wallRayCheck;
+
+public:
+
+	// Standard Component functions
+	virtual void InitializeComponent() override;
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+	
+	virtual void OnMovementModeChanged(EMovementMode PrevMovementMode, uint8 PreviousCustomMode = 0) override;
+
+	virtual void PhysCustom(float deltaTime, int32 Iterations) override;
+
+// This needs to be replaced by an arrow component
+	FVector FacingDirection;
+	void SetFacingDirection(FVector Value); // This could probably be a float but whatever
+
+	// Controls passed to Movement Component
+	void MoveRightInput(float Value);
+	void MoveUpInput(float Value);
+
+	
+	UPROPERTY(EditAnywhere)
+	float WallSlideFriction;
+
+	// JUMP
+
+	UPROPERTY(EditAnywhere)
+	float RisingGravityScalar = 3.5f;
+	UPROPERTY(EditAnywhere)
+	float FallingGravityScalar = 8.f;
+
+	bool isJumping = false;
+	
+	void SetCharacterGravity(float NewGravity);
+	float JumpVelocity = 1750.f;
+	UFUNCTION(BlueprintCallable)
+	void OnJumpInput();
+	
+	UFUNCTION(BlueprintCallable)
+	void BackDash();
+	UFUNCTION(BlueprintCallable)
+	void SetCustomMovementMode(uint8 custom);
+	
+	// DAMAGE
+
+	UFUNCTION(BlueprintCallable)
+	void KnockBack(const FHitResult& Hit);
+	UPROPERTY(EditAnywhere)
+	FVector KnockBackVelocity;
+	
+	// Climb
+	void PhysClimb(float DeltaTime, int32 Iterations);
+	bool IsClimbing() const;
+
+
+	// Grind
+	void PhysGrind(float DeltaTime, int32 Iterations);
+	bool IsGrinding() const;
+
 };
